ede9f3914b38978d507bebc6c338cebbb6124cba
storm5177337 <161874999+storm5177337@users.noreply.github.com>
2024-05-16 13:38:59 -0300

    part 2 done.

--- MapApp/script.js
+++ MapApp/script.js
@@ -11,4 +11,45 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+//navigator code
+
+navigator.geolocation.getCurrentPosition(
+    function (position) {
+        console.log(position);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude]
+        var map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+            .openPopup();
+        //console.log(map)
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent)
+            const lat = mapEvent.latlng.lat
+            const lng = mapEvent.latlng.lng
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                }))
+                .setPopupContent('Workout')
+                .openPopup();
+        })
+    },
+    function () {
+        alert("Could not get position.")
+    }
+);
+
 
